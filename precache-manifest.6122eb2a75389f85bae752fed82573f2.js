5d547cf280722782f162925f2b9651d6d130768f
Viktor-Grom <psn_kirill@icloud.com>
2020-06-13 00:47:41 +0400

    Updates

--- precache-manifest.6122eb2a75389f85bae752fed82573f2.js
+++ precache-manifest.6122eb2a75389f85bae752fed82573f2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d4ea3944bc57045b8c401cf3969f43f4",
+    "revision": "9e2eefbe66e36772cc3b3c76cdf732f7",
     "url": "./index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.54da00bf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e1a34a6613e484cfc2f9",
-    "url": "./static/js/main.58f8e0a7.chunk.js"
+    "revision": "56568f472293d9e5bda6",
+    "url": "./static/js/main.669090c2.chunk.js"
   },
   {
     "revision": "8fdcceb037408d7eaccd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1e6e42a2c91be7cde0317fc5763bcbc0.js"
+  "./precache-manifest.6122eb2a75389f85bae752fed82573f2.js"
 );
 
 self.addEventListener('message', (event) => {
